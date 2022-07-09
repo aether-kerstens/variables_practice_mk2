b7b244a012b19d38ad504cf929347d597769a258
Aether Kerstens <philippekerstens@gmail.com>
2022-07-08 22:16:13 -0700

    Added reassignment and statements

--- variables.rb
+++ variables.rb
@@ -1,16 +1,29 @@
 # strings
-current_us_president = "Joe Biden"
-current_us_traitor = "Donald Trump and the Republican Party"
+us_president = "Joe Biden"
+popular_foods = "cheese"
 first_words = "Hello World"
+
 # integers
 the_meaning_of_life = 42
 omnissiah = 12
 star_wars_year = 1977
+
 # floats
 pi = 3.14
 golden_ratio = 1.61
 eulers_number = 2.71
+
 # booleans
 obi_wan_is_a_jedi = true
 russia_commits_warcrimes = true
 god_emperor_is_god = false
+
+# strings reassignment
+us_president = "Theodore Roosevelt"
+popular_foods = "haggis"
+first_words = "In a hole in the ground there lived a hobbit."
+
+# puts statements
+puts("What is the the_meaning_of_life?", the_meaning_of_life)
+puts("What is pi?", pi)
+puts("What is not a popular food?", popular_foods)
