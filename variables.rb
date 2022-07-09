a13cfce121a1d54cfb820c0fad596302df9e0219
Aether Kerstens <philippekerstens@gmail.com>
2022-07-08 22:04:23 -0700

    Got overeager, did steps 7 - 14 in one go

---
+++ variables.rb
@@ -0,0 +1,16 @@
+# strings
+current_us_president = "Joe Biden"
+current_us_traitor = "Donald Trump and the Republican Party"
+first_words = "Hello World"
+# integers
+the_meaning_of_life = 42
+omnissiah = 12
+star_wars_year = 1977
+# floats
+pi = 3.14
+golden_ratio = 1.61
+eulers_number = 2.71
+# booleans
+obi_wan_is_a_jedi = true
+russia_commits_warcrimes = true
+god_emperor_is_god = false
